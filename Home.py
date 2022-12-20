772f1e2afdf7dcc746b83442d31ad4903a0025ce
tada3345 <tada3345@gmail.com>
2022-12-21 07:34:34 +0900

    Add configuration files

--- Home.py
+++ Home.py
@@ -3,16 +3,19 @@ import functions
 
 todos = functions.get_todos()
 
+st.set_page_config(layout="wide")
+
 def add_todo():
     todo = st.session_state["new_todo"]+'\n'
     todos.append(todo)
     functions.write_todos(todos)
 
-
-
 st.title('Todo アプリ')
 st.subheader('パイソンで作成しました。')
-st.write('作：安田忠邦')
+
+st.write('<b>作：安田忠邦　<b>2022年12月クリマス</b>', unsafe_allow_html=True)
+
+st.text_input(label="新しいtodo", placeholder="新しい項目を追加してください", on_change=add_todo, key="new_todo")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -22,4 +25,3 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.experimental_rerun()
 
-st.text_input(label="新しいtodo", placeholder="新しい項目を追加してください", on_change=add_todo, key="new_todo")
