83b182d6d2751c9045f2107bd14c6b47fcb86e2f
tada3345 <tada3345@gmail.com>
2022-12-20 22:26:44 +0900

    initial commit

---
+++ web.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+def add_todo():
+    todo = st.session_state["new_todo"]+'\n'
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
+
+st.title('Todo アプリ')
+st.subheader('パイソンで作成しました。')
+st.write('作：安田忠邦')
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
+st.text_input(label="新しいtodo", placeholder="新しい項目を追加してください", on_change=add_todo, key="new_todo")
